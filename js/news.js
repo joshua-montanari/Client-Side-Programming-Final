9a80c7fc0752e067408fe4e4f007ca0fcd52a0fe
unknown <joshua.montanari@gmail.com>
2019-12-05 04:37:01 -0500

    finished login and news

---
+++ js/news.js
@@ -0,0 +1,17 @@
+$("#article-1-btn").on("click", () => {
+    $("#article-1-text").css("display", "block");
+    $("#article-2-text").css("display", "none");
+    $("#article-3-text").css("display", "none");
+});
+
+$("#article-2-btn").on("click", () => {
+    $("#article-1-text").css("display", "none");
+    $("#article-2-text").css("display", "block");
+    $("#article-3-text").css("display", "none");
+});
+
+$("#article-3-btn").on("click", () => {
+    $("#article-1-text").css("display", "none");
+    $("#article-2-text").css("display", "none");
+    $("#article-3-text").css("display", "block");
+});
